4e16fa506d14eb3cf05f0ade8532de54914cdb8f
vgt3j4d4 <vgt3j4d4@gmail.com>
2024-08-01 17:35:29 -0500

    improving index.js so files generated under challenges/dist folder will be copied to /submissions folder and an index.html file will be generated with links to those copied folders

--- submissions/index.js
+++ submissions/index.js
@@ -1,14 +1,32 @@
-var fs = require("fs");
-var join = require("path").join;
-var platform = require("os").platform;
-var cp = require("child_process");
-var npmCmd = platform().startsWith("win") ? "npm.cmd" : "npm";
-var challengesPath = join(__dirname, "../challenges");
+const fs = require("fs");
+const join = require("path").join;
+const platform = require("os").platform;
+const cp = require("child_process");
+const npmCmd = platform().startsWith("win") ? "npm.cmd" : "npm";
+const challengesPath = "../challenges";
+const copyFiles = require("copyfiles");
 
+const indexHtml = `<!DOCTYPE html>
+<html>
+<head>
+  <meta charset='utf-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <title>Frontend Mentor Challenges</title>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+<body>
+  <h1>Frontend Mentor Challenges</h1>
+  <ul>{{placeholder}}</ul>
+</body>
+</html>`;
+
+let filePath,
+  generatedDistFolderPath,
+  urls = [];
 console.log("Building all challenges...");
 fs.readdirSync(challengesPath, { withFileTypes: true }).forEach((file) => {
   if (file.isDirectory()) {
-    var filePath = join(challengesPath, file.name);
+    filePath = join("../challenges", file.name);
     // Ensure path has package.json
     if (fs.existsSync(join(filePath, "package.json"))) {
       console.log("Running 'npm run dist' on " + file.name);
@@ -18,5 +36,33 @@ fs.readdirSync(challengesPath, { withFileTypes: true }).forEach((file) => {
         stdio: "inherit",
       });
     }
+    generatedDistFolderPath = join(filePath, "dist", "/**/*");
+    console.log(
+      "Copying files from " + generatedDistFolderPath + " to " + __dirname
+    );
+    copyFiles(
+      [generatedDistFolderPath, join(__dirname, file.name)],
+      { all: true, up: 4 },
+      () => {
+        console.log("Files copied successfully");
+      }
+    );
+    urls.push(file.name);
+  }
+  if (urls.length > 0) {
+    links = [];
+    urls.map((url) => {
+      const label = url
+        .replace(/\-/g, " ")
+        .split(" ")
+        .map((word) => word[0].toUpperCase() + word.slice(1))
+        .join(" ");
+      links.push(`<li><a href="/${url}">${label}</a></li>`);
+    });
+    const indexHtmlContent = indexHtml.replace(
+      "{{placeholder}}",
+      links.join("")
+    );
+    fs.writeFileSync(join(__dirname, "index.html"), indexHtmlContent);
   }
 });
